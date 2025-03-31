3d825a639a24e497892d86a87fcd634be7df7192
ofek29 <ofek.av95@gmail.com>
2025-04-01 00:43:15 +0300

    Add client specific origin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ import config from './config/config.js';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
