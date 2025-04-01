b0b0f8120e46ce5fa88c1ef274a433834781952f
ofek29 <ofek.av95@gmail.com>
2025-04-01 15:45:03 +0300

    Add JSDocs and Change naming

--- server/services/timeService.js
+++ server/services/timeService.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 
-export const getTime = async () => {
+/**
+ * Fetches the current date and time in the Europe/Berlin timezone
+ * from the timeapi.io API.
+ *
+ * @async
+ * @function
+ * @returns {Promise<{
+*   year: number,
+*   month: number,
+*   day: number,
+*   hour: number,
+*   minute: number,
+*   seconds: number,
+*   milliSeconds: number,
+*   dateTime: string,
+*   date: string,
+*   time: string,
+*   timeZone: string,
+*   dayOfWeek: string,
+*   dstActive: boolean
+* }>} Resolves with an object containing detailed date and time information.
+*
+*/
+export const getBerlinTime = async () => {
     const response = await axios.get("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Berlin");
     return response.data;
-}
+};
