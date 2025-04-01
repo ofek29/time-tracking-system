9b6e2f236de8d415b18dd7ebe5024d3f65e4f65e
ofek29 <ofek.av95@gmail.com>
2025-04-01 12:45:49 +0300

    Separate time service

--- server/controllers/timeController.js
+++ server/controllers/timeController.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { getTime } from "../services/timeService";
 
 export const getTime = async (req, res) => {
     try {
-        const response = await axios.get("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Berlin");
+        const response = await getTime();
         res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch time data" });
